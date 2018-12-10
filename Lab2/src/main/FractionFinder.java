4e6a2959538d0e00e15b930437ef5c8986c21765
Robin <Robin@Robin-Dator>
2018-12-10 16:57:40 +0100

    final touches

--- Lab2/src/main/FractionFinder.java
+++ Lab2/src/main/FractionFinder.java
@@ -15,24 +15,26 @@ public class FractionFinder {
 	 * @return Maximum value.
 	 */
 	public static Number findMaxFraction(Number[] list) {
-		
-		//Tuple<Number,Number>[] tuples = convertToTuples(list);
 		if (list.length < 2) {
 			throw new IllegalArgumentException("array size must be larger or equal to 2.");
 		}
 		
+		// If the array length is uneven: "Remove" the last element from the algorithm.
 		int maxIndex = list.length-1;
-		if ((list.length>>1)<<1 != list.length) {
+		boolean unevenFlag = false;
+		if (list.length%2 == 1) { // if the length is uneven.
 			maxIndex--;
+			unevenFlag = true;
 		}
 		
 		Tuple<Number,Tuple<Number,Number>> result = fractionHelper(list, 0, maxIndex);
 		Number frac = result.x;
-		if (list[list.length-1].doubleValue()/result.y.x.doubleValue() > frac.doubleValue()) {
+		
+		// Code for when the array length is uneven.
+		if (unevenFlag && (list[list.length-1].doubleValue()/result.y.x.doubleValue() > frac.doubleValue())) {
 			frac = list[list.length-1].doubleValue()/result.y.x.doubleValue();
 		}
-		//return result.y.doubleValue()/result.x.doubleValue();
-		//return result.x.doubleValue() > result.y.y.doubleValue()/result.y.x.doubleValue() ? result.y.y.doubleValue()/result.y.x.doubleValue() : result.x;
+		
 		return frac;
 	}
 	
@@ -45,8 +47,7 @@ public class FractionFinder {
 	 */
 	private static Tuple<Number, Tuple<Number,Number>> fractionHelper(Number[] list, int lowIndex, int highIndex) {
 		if (highIndex == (lowIndex+1)) {	// Base case.
-			//System.out.println(list[lowIndex]);
-			//return list[lowIndex];		// One pair equals two elements.
+			// Calculate lowest and largest numbers and put them in a tuple.
 			Tuple<Number,Number> lowHigh;
 			if (list[lowIndex].doubleValue() < list[highIndex].doubleValue()) {
 				lowHigh = new Tuple<Number,Number>(list[lowIndex], list[highIndex]);
@@ -54,50 +55,32 @@ public class FractionFinder {
 				lowHigh = new Tuple<Number,Number>(list[highIndex], list[lowIndex]);
 			}
 			
-			return new Tuple<Number,Tuple<Number,Number>>(list[highIndex].doubleValue()/list[lowIndex].doubleValue(),lowHigh);
+			// Calculate the fraction.
+			Number fraction = list[highIndex].doubleValue()/list[lowIndex].doubleValue();
+			
+			return new Tuple<Number,Tuple<Number,Number>>(fraction,lowHigh);
 		}
 		
 		// Recursion.
 		Tuple<Number, Tuple<Number,Number>> tuple1 = fractionHelper(list, lowIndex,(((highIndex-lowIndex+1)>>2)<<1)-1+lowIndex);
 		Tuple<Number, Tuple<Number,Number>> tuple2 = fractionHelper(list, (((highIndex-lowIndex+1)>>2)<<1)+lowIndex, highIndex);
 		
-		Number value = tuple1.x.doubleValue() > tuple2.x.doubleValue() ? tuple1.x.doubleValue() : tuple2.x.doubleValue();
-		Number frac = tuple2.y.y.doubleValue() / tuple1.y.x.doubleValue();
-		value = value.doubleValue() < frac.doubleValue() ? frac : value;
+		// Get the largest local fraction
+		Number maxFraction = tuple1.x.doubleValue() > tuple2.x.doubleValue() ? tuple1.x.doubleValue() : tuple2.x.doubleValue();
 		
+		// Get the largest/smallest fraction.
+		Number tempFrac = tuple2.y.y.doubleValue() / tuple1.y.x.doubleValue();
+		
+		// Calculate the new global max fraction.
+		maxFraction = maxFraction.doubleValue() < tempFrac.doubleValue() ? tempFrac : maxFraction;
+		
+		// Calculate global max and min numbers.
 		Number lowest = tuple1.y.x.doubleValue() < tuple2.y.x.doubleValue() ? tuple1.y.x : tuple2.y.x;
 		Number highest = tuple1.y.y.doubleValue() > tuple2.y.y.doubleValue() ? tuple1.y.y :tuple2.y.y;
 		
+		// Create a pair with global min and max numbers.
 		Tuple<Number,Number> result = new Tuple<Number,Number>(lowest, highest);
 
-		return new Tuple<Number,Tuple<Number,Number>>(value,result);
+		return new Tuple<Number,Tuple<Number,Number>>(maxFraction,result);
 	}
-	
-	/**
-	 * Create an array of pairs instead of numbers. Now we can compare pairs instead ;)
-	 * @param array
-	 * @return an array of pairs representing the input.
-	 */
-	@Deprecated
-	private static Tuple<Number, Number>[] convertToTuples(Number[] array) {
-		int isOdd = array.length/2 == array.length/2.0f ? 0 : 1;	// we need this if the list length is odd.
-		
-		
-		@SuppressWarnings("unchecked")
-		Tuple<Number,Number>[] tuples = (Tuple<Number, Number>[]) new Tuple[array.length/2 + isOdd];
-		
-		// copy all* elements.
-		for (int i = 0; i < array.length/2; i++) {
-			tuples[i] = new Tuple<Number,Number>(array[i*2],array[i*2+1]);
-		}
-		
-		// When the list is odd we put the last element together with the second last.
-		if (isOdd == 1) {
-			tuples[tuples.length-1] = new Tuple<Number,Number>(array[array.length-2],array[array.length-1]);
-		}
-		return tuples;
-		
-		// * Don't copy the last element if the list is odd. This is done separate.
-	}
-
 }
--- Lab2/src/main/MainTester.java
+++ Lab2/src/main/MainTester.java
@@ -14,11 +14,16 @@ import jxl.write.WriteException;
 import jxl.write.biff.RowsExceededException;
 import list.NodeList;
 import misc.Tuple;
-import quicksort.QuickSort.PivotPositions;
 
+/**
+ * Testing class for lab2 part 1.
+ * @author Robin, Oskar
+ *
+ */
 public class MainTester {
 	private int intervalSize, iterations;
 	private NodeList<Tuple<Integer, Long>> algorithmResults;
+	
 	/**
 	 * Class used for testing the implementation.
 	 * @author Robin, Oskar
@@ -31,26 +36,18 @@ public class MainTester {
 	
 
 	public static void main(String[] args) {
-		/*
+		/* Code for testing the functionality of the algorithm.
 		Integer[] array = new Integer[9];
 		Random r = new Random();
 		for (int i = 0; i < array.length; i++) {
 			array[i] = r.nextInt(array.length+1)+1;
 		}
-		array[0] = 5;
-		array[1] = 4;
-		array[2] = 3;
-		array[3] = 2;
-		array[4] = 5;
-		array[5] = 3;
-		array[6] = 2;
-		array[7] = 1;
 		
 		for (int i = 0; i < array.length ; i++) {
-			System.out.println(array[i]);
+			System.out.print(array[i] + ", ");
 		}
 		
-		System.out.println("result: " + FractionFinder.findMaxFraction(array));
+		System.out.println("\nresult: " + FractionFinder.findMaxFraction(array));
 		*/
 		
 		Scanner consoleScanner = new Scanner(System.in);
@@ -67,7 +64,6 @@ public class MainTester {
 		consoleScanner.close();
 		
 		new MainTester(interval, iterations).analyzeListAndExport(expoFile);
-		
 	}
 	
 	
@@ -133,29 +129,15 @@ public class MainTester {
 		sheet.addCell(label);
 		
 		Number number;
-		
-		//Number num;
-		/*for (int i = 1; i <= this.iterations; i++) {
-			row ++;
-			num = new Number(column, row, i*this.intervalSize);
-			sheet.addCell(num);
-		}*/
-		
 
 		// for every pivot: add all data.
 		for (Tuple<Integer, Long> tuple : this.algorithmResults) {
-			
-			//row = 0;
-			//column += 1;
-			//label = new Label(column,row,tup.x.toString());
-			//sheet.addCell(label);
-
-				row ++;
-				number = new Number(column, row, tuple.x);
-				sheet.addCell(number);
+			row ++;
+			number = new Number(column, row, tuple.x);
+			sheet.addCell(number);
 				
-				number = new Number(column+1, row, tuple.y);
-				sheet.addCell(number);
+			number = new Number(column+1, row, tuple.y);
+			sheet.addCell(number);
 		}
 		
 		// Note to self: Remember to write and close.
